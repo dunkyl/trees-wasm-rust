5448bc029cb2deee71d555ce3d81e236b00f9694
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2023-04-11 18:16:03 -0700

    try emscripten

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 use wasmer::{Store, Module, Instance, Value, imports, EngineBuilder, Memory, MemoryType, Pages};
 use wasmer::FunctionEnv;
-use wasmer_compiler_cranelift::Cranelift;
+use wasmer_emscripten::*;
 
 fn main() -> anyhow::Result<()> {
 
-    let compiler_config = Cranelift::default();
-    let engine = EngineBuilder::new(compiler_config);
-    let engine = engine.engine();
+    // let compiler_config = Cranelift::default();
+    // let engine = EngineBuilder::new(compiler_config);
+    // let engine = engine.engine();
 
 
-    let mut store = Store::new(&engine);
+    // let mut store = Store::new(&engine);
+    let mut store = Store::default();
+
+    let rust_wasm = std::fs::read("./tree-sitter-rust.wasm")?;
+
+    let env = wasmer_emscripten::EmEnv::default();
+
+    
     
     let start = std::time::Instant::now();
 
-    let module = Module::from_file(&engine, "./rust.wasm")?;
+    // let module = Module::from_file(&engine, "./rust.wasm")?;
+    let module = Module::new(&store, rust_wasm)?;
+
+
+    
 
     println!("Module load time: {:?}", start.elapsed());
     
     // The module doesn't import anything, so we create an empty import object.
     let import_object = imports! {};
-    let instance = Instance::new(&mut store, &module, &import_object)?;
+    let mut instance = Instance::new(&mut store, &module, &import_object)?;
+
+    let fnenv = FunctionEnv::new(&mut store, wasmer_emscripten::EmEnv::new());
+    let mut emscripten_globals = EmscriptenGlobals::new(&mut store, &fnenv, &module).unwrap();
+    
+    run_emscripten_instance(&mut instance, fnenv.into_mut(&mut store), &mut emscripten_globals, ".", vec![], None)?;
 
 
     let trees_rust = instance.exports.get_function("tree_sitter_rust")?;
